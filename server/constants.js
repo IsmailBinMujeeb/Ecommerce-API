a389f063a11bf65bfd47c1dd697d1a58cac34d05
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-06-03 20:45:51 +0530

    add constants.js

---
+++ server/constants.js
@@ -0,0 +1,24 @@
+export const MODERATOR_PERMISSIONS = {
+    can_view_products: "can_view_products",
+    can_view_categories: "can_view_categories",
+    can_view_payments: "can_view_payments",
+    can_view_orders: "can_view_orders",
+    can_view_user_info: "can_view_user_info",
+    can_view_review: "can_view_review",
+    can_view_admin_dashboard: "can_view_admin_dashboard",
+
+    can_manage_personal_cart: "can_manage_personal_cart",
+    can_place_orders: "can_place_orders",
+    can_write_reviews: "can_write_reviews",
+
+    can_moderate_reviews: "can_moderate_reviews",
+    can_moderate_products: "can_moderate_products",
+
+    can_perform_crud_on_product: "can_perform_crud_on_product",
+    can_perform_crud_on_category: "can_perform_crud_on_category",
+
+    can_ban_user: "can_ban_user",
+    can_promote_user: "can_promote_user",
+};
+
+export const AVAILABLE_MODERATOR_PERMISSIONS = Object.values(MODERATOR_PERMISSIONS);
