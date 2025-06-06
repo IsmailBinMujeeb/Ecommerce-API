50461e4a346e0a6a94ab925102f97c8684091a46
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-06-06 17:11:57 +0530

    update server js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,13 @@ import { config } from "dotenv";
 
 config();
 
+import authRouter from "./routes/api/auth.route.js";
 import userRouter from "./routes/api/user.route.js";
 import cartRouter from "./routes/api/cart.route.js";
+import productRouter from "./routes/api/product.route.js";
+import categoryRouter from "./routes/api/category.route.js";
+import orderRouter from "./routes/api/order.route.js";
+import reviewRouter from "./routes/api/review.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,7 +22,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
+app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/product", productRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/review", reviewRouter);
 
 export default app;
