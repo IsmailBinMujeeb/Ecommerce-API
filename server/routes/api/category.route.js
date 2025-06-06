6e5f443bcea621946fce39781fce3bf9c9d2d8e4
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-06-06 21:49:24 +0530

    add validation on route for fetch category

--- server/routes/api/category.route.js
+++ server/routes/api/category.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { FetchAllCategories, CreateCategory, DeleteCategory, FetchCategory, UpdateCategory } from "../../controllers/api/category.controller.js";
-import { categoryIdParamValidator, createCategoryValidator, updateCategoryValidator } from "../../validators/category.validator.js";
+import { categoryIdParamValidator, createCategoryValidator, updateCategoryValidator, FetchCategoryValidator } from "../../validators/category.validator.js";
 import validatorMiddleware from "../../middlewares/validator.middleware.js";
 import { MODERATOR_PERMISSIONS } from "../../constants.js";
 import authenticationMiddleware from "../../middlewares/authentication.middleware.js";
@@ -11,7 +11,7 @@ import AsyncHandler from "../../utils/AsyncHandler.utils.js";
 const router = Router();
 
 router.get("/", AsyncHandler(FetchAllCategories));
-router.get("/:id", AsyncHandler(FetchCategory));
+router.get("/:id", FetchCategoryValidator(), validatorMiddleware, AsyncHandler(FetchCategory));
 router.post("/", authenticationMiddleware, permissionsMiddleware(MODERATOR_PERMISSIONS.can_perform_crud_on_category), createCategoryValidator(), validatorMiddleware, AsyncHandler(CreateCategory));
 router.put("/:id", authenticationMiddleware, permissionsMiddleware(MODERATOR_PERMISSIONS.can_perform_crud_on_category), categoryIdParamValidator(), updateCategoryValidator(), validatorMiddleware, AsyncHandler(UpdateCategory));
 router.delete("/:id", authenticationMiddleware, permissionsMiddleware(MODERATOR_PERMISSIONS.can_perform_crud_on_category), categoryIdParamValidator(), validatorMiddleware, AsyncHandler(DeleteCategory));
